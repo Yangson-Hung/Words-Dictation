939a732647c9139c963bab6fa87a0ac6d75e6051
Yangson <2545594421@qq.com>
2019-01-01 19:45:45 +0800

    update

--- tts.h
+++ tts.h
@@ -5,21 +5,21 @@
 /* wav音频头部格式 */
 typedef struct _wave_pcm_hdr
 {
-    char            riff[4];                // = "RIFF"
-    int				size_8;                 // = FileSize - 8
-    char            wave[4];                // = "WAVE"
-    char            fmt[4];                 // = "fmt "
-    int				fmt_size;				// = 下一个结构体的大小 : 16
+    char            riff[4];
+    int             size_8;
+    char            wave[4];
+    char            fmt[4];
+    int             fmt_size;
 
-    short int       format_tag;             // = PCM : 1
-    short int       channels;               // = 通道数 : 1
-    int				samples_per_sec;        // = 采样率 : 8000 | 6000 | 11025 | 16000
-    int				avg_bytes_per_sec;      // = 每秒字节数 : samples_per_sec * bits_per_sample / 8
-    short int       block_align;            // = 每采样点字节数 : wBitsPerSample / 8
-    short int       bits_per_sample;        // = 量化比特数: 8 | 16
+    short int       format_tag;
+    short int       channels;
+    int             samples_per_sec;
+    int             avg_bytes_per_sec;
+    short int       block_align;
+    short int       bits_per_sample;
 
-    char            data[4];                // = "data";
-    int				data_size;              // = 纯数据长度 : FileSize - 44
+    char            data[4];
+    int             data_size;
 } wave_pcm_hdr;
 
 int text_to_speech(const char* src_text, const char* des_path, const char* params);

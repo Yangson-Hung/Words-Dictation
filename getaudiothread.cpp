89bf6b581d22f10d2cf12f685e46113e6111b6cc
Yangson <2545594421@qq.com>
2019-01-03 16:15:53 +0800

    update

---
+++ getaudiothread.cpp
@@ -0,0 +1,17 @@
+﻿#include "getaudiothread.h"
+#include "tts.h"
+#include "dictation.h"
+
+
+GetAudioThread::GetAudioThread(QVector<QString> &in_qstr_vector, QString &in_voice_name)
+{
+    qstr_vector = in_qstr_vector;
+    voice_name = in_voice_name;
+}
+
+void GetAudioThread::run()
+{
+    for (int i = 0; i < qstr_vector.size(); ++i) {
+        begin_tts(qstr_vector[i],qstr_vector[i],voice_name);
+    }
+}
